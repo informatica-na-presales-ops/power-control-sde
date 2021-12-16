417ce67d4cfa08409a904081c0181a4b69c4e51a
William Jackson <william@subtlecoolness.com>
2021-12-16 11:03:38 -0600

    Add logging and correct region

--- power-control-sde.py
+++ power-control-sde.py
@@ -1,29 +1,35 @@
-import apscheduler
+import apscheduler.schedulers.blocking
 import boto3
+import notch
 import os
+
+log = notch.make_log('power_control_sde')
 
 
 def get_instances():
     # INSTANCE_IDS="i-12345,i-23456"
     instance_ids = os.getenv('INSTANCE_IDS').split(',')
+    region = os.getenv('AWS_REGION', 'us-west-2')
 
-    ec2 = boto3.resource('ec2')
+    ec2 = boto3.resource('ec2', region_name=region)
     instances = ec2.instances.filter(InstanceIds=instance_ids)
     return instances
 
 
 def start_instances():
     for instance in get_instances():
+        log.info(f'Starting instance {instance.id}')
         instance.start()
 
 
 def stop_instances():
     for instance in get_instances():
+        log.info(f'Stopping instance {instance.id}')
         instance.stop()
 
 
 if __name__ == '__main__':
     scheduler = apscheduler.schedulers.blocking.BlockingScheduler()
-    scheduler.add_job(start_instances, 'cron', day_of_week='0-4', hour=7, minute=0)
-    scheduler.add_job(stop_instances, 'cron', hour=21, minute=0)
+    scheduler.add_job(start_instances, 'cron', day_of_week='0-4', hour=4, minute=0)
+    scheduler.add_job(stop_instances, 'cron', hour=19, minute=0)
     scheduler.start()
